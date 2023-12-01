06ccf0148f9be30a8a74aa6a20f629ae2d37d386
Georg Stockinger <pangeorg@gmx.de>
2023-12-01 17:11:51 +0100

    more more more

--- FackyWackyExtensions.cs
+++ FackyWackyExtensions.cs
@@ -17,6 +17,7 @@ public static class FackyWackyExtensions
         return collection.Remove(item);
     }
 
+
     public static void GetTheFuckIn<T>(this ICollection<T> collection, T item)
     {
         collection.Add(item);
@@ -27,6 +28,11 @@ public static class FackyWackyExtensions
         collection.CopyTo(array, arrayIndex);
     }
 
+    public static bool GetTheFuckOut<T>(this IList<T> list, T item)
+    {
+        return list.Remove(item);
+    }
+
     public static int WhereTheFuckIsThis<T>(this IList<T> list, T elem)
     {
         return list.IndexOf(elem);
@@ -35,6 +41,39 @@ public static class FackyWackyExtensions
     public static void GetTheFuckOutSpecific<T>(this IList<T> list, int index)
     {
         list.RemoveAt(index);
+    }
+
+    public static void ReleaseMyself<T>(this IList<T> list)
+    {
+        list.Clear();
+    }
+
+    public static T GimmeThat<T>(this IEnumerable<T> enumerable, int index)
+    {
+        return enumerable.ElementAt(index);
+    }
+
+    public static T GimmeThat<T>(this IEnumerable<T> enumerable, Index index)
+    {
+        return enumerable.ElementAt(index);
+    }
+
+    public static T? GimmeThatOrWhatever<T>(this IEnumerable<T> enumerable, int index)
+    {
+        return enumerable.ElementAtOrDefault(index);
+    }
+
+    public static T? GimmeThatOrWhatever<T>(this IEnumerable<T> enumerable, Index index)
+    {
+        return enumerable.ElementAtOrDefault(index);
+    }
+
+    public static IEnumerable<IGrouping<TKey,TSource>> CuddlePuddleBy<TKey, TSource>(
+        this IEnumerable<TSource> source,
+        Func<TSource,TKey> keySelector,
+        IEqualityComparer<TKey>? comparer)
+    {
+        return source.GroupBy(keySelector, comparer);
     }
 }
 
